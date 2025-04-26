7b5b5d2c59747c4f12fc4bf8c987b36253fa0099
Roger Blanco <rogerblanco10@gmail.com>
2025-04-26 13:42:51 +0200

    use real data from the sensor in the graph

--- front/main.js
+++ front/main.js
@@ -13,26 +13,40 @@ const chart = Highcharts.chart('temperature-chart', {
     },
     series: [{
         showInLegend: false,
-        data: [
-            [10, 20],
-            [11, 22],
-            [12, 30],
-            [13, 25],
-        ]
+        data: [],
     }],
     credits: {
         enabled: false
     },
 });
 
-// Simulate new data points
-setTimeout(() => {
-    chart.series[0].addPoint([14, 28], animation=true);
-    chart.series[0].addPoint([15, 26], animation=true);
+function roundNumber(num, n) {
+    return Math.round(num * Math.pow(10, n)) / Math.pow(10, n);
+}
+
+const URL = 'http://192.168.0.73';
+
+async function updateChart() {
+    const url = `${URL}/temperature`;
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+        });
+        const data = await response.json();
+        console.log(data);
+
+        if(data.temperature) {
+            const temperature = roundNumber(data.temperature, 2);
+            const time = new Date().getTime();
+            chart.series[0].addPoint([time, temperature], animation=true);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+updateChart();
+// every 2 seconds
+setInterval(() => {
+    updateChart();
 }, 2000);
-setTimeout(() => {
-    chart.series[0].addPoint([16, 28], shift=true, animation=true);
-}, 4000);
-setTimeout(() => {
-    chart.series[0].addPoint([17, 26], animation=true);
-}, 8000);
